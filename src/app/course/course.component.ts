1bc6ae9ad4765c8782404c8d691c6948c9cf277e
rajadurai2497 <rajadurai2497@gmail.com>
2020-07-05 16:50:21 +0530

    course-details-page-completed

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from '../services/course.service';
 import { AllCourse } from '../models/course/course.model';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.scss'],
-  providers :[CourseService]
+  providers: [CourseService]
 })
 export class CourseComponent implements OnInit {
 
   allCourse: AllCourse[] = [];
-  constructor(private readonly _courseService: CourseService) { }
+  constructor(private readonly _courseService: CourseService, private router: Router) { }
 
-  
+
 
   ngOnInit(): void {
     this.getAllCourselist();
@@ -25,10 +26,20 @@ export class CourseComponent implements OnInit {
       }
     });
   }
-  public goToDetails(course): void {
-    this._courseService.currentCourse=course;
+  public goToDetails(course: AllCourse): void {
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        "courseId": course.courseMasterId,
+        "courseName": course.courseName,
+        "courseAmount": course.courseAmount,
+        "provideWhat": course.provideWhat,
+        "description": course.description,
+      },
+      skipLocationChange: true
+    };
+
+    this.router.navigate(['course-details'], navigationExtras);
   }
 }
 
 
-  
