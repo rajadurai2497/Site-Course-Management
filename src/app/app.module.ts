cbe81711c52c63cae262034b9b0fcb0fd8321c62
rajadurai2497 <rajadurai2497@gmail.com>
2020-07-05 20:08:21 +0530

    format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,9 @@ import { SignupComponent } from './signup/signup.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { HomeModule } from './home/home.module';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { CommunityComponent } from './community/community.component';
-import { QueriesComponent } from './queries/queries.component';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
-import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CourseComponent } from './course/course.component';
-import { CoursesModule } from './courses/courses.module';
 import { HttpClientModule } from '@angular/common/http';
-import { CourseDetailsComponent } from './courses/course-details/course-details.component';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 @NgModule({
