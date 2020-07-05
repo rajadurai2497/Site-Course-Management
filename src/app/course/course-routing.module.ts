7a392d112a43f1f215019bf62fefa03e01072a45
rajadurai2497 <rajadurai2497@gmail.com>
2020-07-05 19:47:26 +0530

    lazy-load

---
+++ src/app/course/course-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CommunityComponent } from '../community/community.component';
+
+
+const routes: Routes = [{
+  path: '',
+  component: CommunityComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CourseRoutingModule { }
