3243076ea180fcd490c10a405a6a9ad036d737ba
Beena2104 <beenadasc@gmail.com>
2020-07-06 10:53:16 +0530

    val

--- src/app/queries/queries.component.ts
+++ src/app/queries/queries.component.ts
@@ -14,6 +14,7 @@ export class QueriesComponent implements OnInit {
   addQueries: any;
   emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
   phoneNumberPattern = '^((\\+91-?)|0)?[0-9]{10}$';
+  dialogRef: any;
   constructor(private readonly queriesService: QueriesService) {}
 
   ngOnInit(): void  {
@@ -30,20 +31,21 @@ export class QueriesComponent implements OnInit {
         Validators.required,
         Validators.pattern(this. phoneNumberPattern),
       ]),
-      alterEgo: new FormControl(this.queries.alterEgo),
-      power: new FormControl(this.queries.power, Validators.required)
     });
    }
    get fullName() { return this.queries.get('fullName'); }
-  get power() { return this.queries.get('power'); }
   get emailId() { return this.queries.get('emailId'); }
   get phoneNumber() { return this.queries.get('phoneNumber'); }
 
   public onSubmitButtonClicked(): void {
     this.queries.contactusId = 0;
     this.queriesService.createQueries(this.queries).subscribe((data) => {
-      alert('Registered Successfully');
-      console.log('Add');
+      if (data && data.result) {
+        alert('Registered Successfully');
+        } else {
+          alert('Registered UnSuccessfully');
+        }
+      this.dialogRef.close(true);
     });
   }
 }
