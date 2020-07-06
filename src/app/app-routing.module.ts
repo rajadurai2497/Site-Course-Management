de0b9cb4272f0b12708779652bbda90e9c3a16fc
rajadurai2497 <rajadurai2497@gmail.com>
2020-07-06 09:43:39 +0530

    hash-add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -57,7 +57,7 @@ const routes: Routes = [
   imports: [CommonModule,
     BrowserModule,
     RouterModule.forRoot(routes, {
-      useHash: true
+      useHash: false
     })],
   exports: [RouterModule]
 })
